ba0672172ea2c759b3c32c10c5ef3f14ff6be183
Tabajdi Szabolcs <h670180@stud.u-szeged.hu>
2020-06-11 13:50:48 +0200

    Using radium for media queries

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
+import Radium , { StyleRoot } from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -46,11 +47,16 @@ class App extends Component {
 
   render(){
     const buttonStyle ={
-        backgroundColor: 'white',
+        backgroundColor: 'green',
+        color: 'white',
         font: 'inherit',
         border: '1px solid blue',
         padding: '8px',
-        cursor: 'pointer'
+        cursor: 'pointer',
+        ':hover': {
+          backgroundColor: 'lightgreen',
+          color: 'black'
+        }
     };
 
     let persons = null;
@@ -68,21 +74,38 @@ class App extends Component {
           })}
         </div> 
       );
+      buttonStyle.backgroundColor = 'red';
+      buttonStyle[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
+    const classes = [];
+
+    if(this.state.persons.length <= 2){
+      classes.push('red'); // classes = ['red']
+    }
+    if(this.state.persons.length <= 1){
+      classes.push('bold'); // classes = ['red', 'bold']
+    }
+
+
     return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        <button
-          style={buttonStyle}
-          onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        {persons}
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, I'm a React App</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button
+            style={buttonStyle}
+            onClick={this.togglePersonsHandler}>Toggle Persons</button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
     //return React.createElement('div', {className:'App'}, React.createElement('h1', null , 'Does this work now?'))
   }
 
 }
 
-export default App;
+export default Radium(App);
